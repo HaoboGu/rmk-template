635b06753e2ecde9f1558afff281737f98efd225
Haobo Gu <haobogu@outlook.com>
2024-01-26 15:45:33 +0800

    feat: make the template usable for stm32/rp
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

---
+++ src/main_rp.rs
@@ -0,0 +1,85 @@
+#![no_main]
+#![no_std]
+
+#[macro_use]
+mod keymap;
+#[macro_use]
+mod macros;
+mod vial;
+
+use crate::keymap::{COL, NUM_LAYER, ROW};
+use core::cell::RefCell;
+use defmt::*;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_rp::{
+    bind_interrupts,
+    flash::{Blocking, Flash},
+    gpio::{AnyPin, Input, Output},
+    peripherals::{self, USB},
+    usb::{Driver, InterruptHandler},
+};
+use panic_probe as _;
+use rmk::{initialize_keyboard_and_run, keymap::KeyMap};
+use static_cell::StaticCell;
+use vial::{VIAL_KEYBOARD_DEF, VIAL_KEYBOARD_ID};
+
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+const EEPROM_SIZE: usize = 128;
+const FLASH_SIZE: usize = 2 * 1024 * 1024;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Rmk start!");
+    // Initialize peripherals
+    let p = embassy_rp::init(Default::default());
+
+    // Create the usb driver, from the HAL
+    let driver = Driver::new(p.USB, Irqs);
+
+    // Pin config
+    let (input_pins, output_pins) = config_matrix_pins_rp!(peripherals: p, input: [PIN_6, PIN_7, PIN_8, PIN_9], output: [PIN_19, PIN_20, PIN_21]);
+
+    // Use internal flash to emulate eeprom
+    let flash = Flash::<_, Blocking, FLASH_SIZE>::new_blocking(p.FLASH);
+    // Keymap + eeprom config
+    static MY_KEYMAP: StaticCell<
+        RefCell<
+            KeyMap<
+                Flash<peripherals::FLASH, Blocking, FLASH_SIZE>,
+                EEPROM_SIZE,
+                ROW,
+                COL,
+                NUM_LAYER,
+            >,
+        >,
+    > = StaticCell::new();
+    let keymap = MY_KEYMAP.init(RefCell::new(KeyMap::new(
+        crate::keymap::KEYMAP,
+        Some(flash),
+        None,
+    )));
+
+    // Initialize all utilities: keyboard, usb and keymap
+    initialize_keyboard_and_run::<
+        Driver<'_, USB>,
+        Input<'_, AnyPin>,
+        Output<'_, AnyPin>,
+        Flash<peripherals::FLASH, Blocking, FLASH_SIZE>,
+        EEPROM_SIZE,
+        ROW,
+        COL,
+        NUM_LAYER,
+    >(
+        driver,
+        input_pins,
+        output_pins,
+        keymap,
+        VIAL_KEYBOARD_ID,
+        VIAL_KEYBOARD_DEF,
+    )
+    .await;
+}
---
+++ src/main_stm32.rs
@@ -0,0 +1,90 @@
+#![no_main]
+#![no_std]
+
+#[macro_use]
+mod macros;
+mod keymap;
+mod vial;
+
+use crate::keymap::{COL, NUM_LAYER, ROW};
+use core::cell::RefCell;
+use defmt::*;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_stm32::{
+    bind_interrupts,
+    flash::{Blocking, Flash},
+    gpio::{AnyPin, Input, Output},
+    peripherals::USB_OTG_FS,
+    usb_otg::{Driver, InterruptHandler},
+    Config,
+};
+use panic_probe as _;
+use rmk::{initialize_keyboard_and_run, keymap::KeyMap};
+use static_cell::StaticCell;
+use vial::{VIAL_KEYBOARD_DEF, VIAL_KEYBOARD_ID};
+
+// TODO: By default Rmk uses USB_OTG_FS. If your microcontroller has only USB_OTF_HS, you have to update all usages below
+bind_interrupts!(struct Irqs {
+    OTG_FS => InterruptHandler<USB_OTG_FS>;
+});
+
+const EEPROM_SIZE: usize = 128;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Rmk start!");
+    // RCC config
+    let config = Config::default();
+
+    // Initialize peripherals
+    let p = embassy_stm32::init(config);
+
+    // Usb config
+    static EP_OUT_BUFFER: StaticCell<[u8; 1024]> = StaticCell::new();
+    let mut usb_config = embassy_stm32::usb_otg::Config::default();
+    usb_config.vbus_detection = false;
+    let driver = Driver::new_fs(
+        p.USB_OTG_FS,
+        Irqs,
+        p.PA12,
+        p.PA11,
+        &mut EP_OUT_BUFFER.init([0; 1024])[..],
+        usb_config,
+    );
+
+    // Pin config
+    let (input_pins, output_pins) = config_matrix_pins_stm32!(peripherals: p, input: [PD9, PD8, PB13, PB12], output: [PE13, PE14, PE15]);
+
+    // Use internal flash to emulate eeprom
+    let f = Flash::new_blocking(p.FLASH);
+    // Keymap + eeprom config
+    static MY_KEYMAP: StaticCell<
+        RefCell<KeyMap<Flash<'_, Blocking>, EEPROM_SIZE, ROW, COL, NUM_LAYER>>,
+    > = StaticCell::new();
+    let keymap = MY_KEYMAP.init(RefCell::new(KeyMap::new(
+        crate::keymap::KEYMAP,
+        Some(f),
+        None,
+    )));
+
+    // Start serving
+    initialize_keyboard_and_run::<
+        Driver<'_, USB_OTG_FS>,
+        Input<'_, AnyPin>,
+        Output<'_, AnyPin>,
+        Flash<'_, Blocking>,
+        EEPROM_SIZE,
+        ROW,
+        COL,
+        NUM_LAYER,
+    >(
+        driver,
+        input_pins,
+        output_pins,
+        keymap,
+        VIAL_KEYBOARD_ID,
+        VIAL_KEYBOARD_DEF,
+    )
+    .await;
+}
